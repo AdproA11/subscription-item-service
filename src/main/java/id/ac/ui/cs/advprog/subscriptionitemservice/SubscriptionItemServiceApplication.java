23211b6c9ff89a4ba3b402558a6d29f286ec2a95
Mario <mario.michaeljs21@gmail.com>
2024-04-18 01:16:45 +0700

    Initialize SpringBoot project

---
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionitemservice/SubscriptionItemServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.subscriptionitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionItemServiceApplication.class, args);
+	}
+
+}
